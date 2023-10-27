f34a06e5d6ad82f27caead7d42f8c898c98a11ab
Adam <awa@omni3d.net>
2023-10-27 13:58:25 +0200

    Added comments for main loop

--- tray_management.py
+++ tray_management.py
@@ -1,11 +1,19 @@
 # Libraries
 import time
 # Modules
-from tray_abstract import tray, Direction
+from tray_abstract import tray
 # Program entry
 
 if __name__ == "__main__":
+    #Configure everything on entry
+
+    # Create tray object
     extruder_0_tray = tray(0)
 
+    #Spin
     while(True):
+        # 1. Check for new commands
+
+        # 2. report trays status
+
         time.sleep(1)
