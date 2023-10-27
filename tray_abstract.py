6fec8ebb0da66081553267b9ee36fd0fb2c36352
Adam <awa@omni3d.net>
2023-10-27 13:55:35 +0200

    Added helper helper class 'move' and enums

--- tray_abstract.py
+++ tray_abstract.py
@@ -8,17 +8,34 @@ from dsf.connections import SubscribeConnection, SubscriptionMode
 import json
 from enum import IntEnum
 
+# Enum class describing direction of motor movement
 class Direction(IntEnum):
-        BACKWARD = -1
-        FORWARD = 1
-# Create abstraction for reading filament sensors and controling tray motors.
-class tray:
+    BACKWARD = -1
+    FORWARD = 1
+# Enum class describing tray sensors
+class Sensors(IntEnum):
+    RIGHT_TRAY_SENSOR = 6
+    LEFT_TRAY_SENSOR = 7
+    EXTRUDER_SENSOR = 8
+# Enum class describing stop condition for motors
+class condition(IntEnum):
+    BY_DISTANCE = 0
+    WAIT_FOR_SENSOR = 1
+# abstract stepper move
+class move:
+    def __init__(self, _condition, _right_motor_distance, _left_motor_distance, _feedrate):
+        self.condition = _condition
+        self.right_motor_distance = _right_motor_distance
+        self.left_motor_distance = _left_motor_distance
+        self.feedrate = _feedrate
 
+# abstraction for filament tray system
+class tray:
     def __init__(self, _tray_number):
         # TODO Initialize with real state
-        self.right_sensor = self.get_sensors_state(6)
-        self.left_sensor = self.get_sensors_state(7)
-        self.tool_sensor = self.get_sensors_state(8)
+        self.right_sensor = self.get_sensors_state(Sensors.RIGHT_TRAY_SENSOR)
+        self.left_sensor = self.get_sensors_state(Sensors.LEFT_TRAY_SENSOR)
+        self.tool_sensor = self.get_sensors_state(Sensors.EXTRUDER_SENSOR)
         self.tray_number = _tray_number
         if _tray_number == 0:
             self.tray_motor_0 = 2
@@ -41,30 +58,29 @@ class tray:
         return object_model["result"]
     def wait_for_sensor_state(sensor, state_to_wait ,timeout):
         return True
-    # Move tray motor by X milimeters in blocking mode
-    def move_motor_by(self, both, motor, dir, distance, feedrate):
-        command_connection = CommandConnection(debug=False)
-        command_connection.connect()
-        # Allow cold extrusion
-        command_connection.perform_simple_code("M302 P1")
-        # Set extruder relative mode
-        command_connection.perform_simple_code("M83")
-        # Select proper motor, or both if specified
-        if both == True:
-            command_connection.perform_simple_code("M563 P0 D{}:{}:{} H{} F{}".format(self.tray_number, self.tray_motor_0, self.tray_motor_1, self.tray_number, self.tray_number))
-        else:
-            command_connection.perform_simple_code("M563 P0 D{}:{} H{} F{}".format(self.tray_number, motor, self.tray_number, self.tray_number))
-        # Select tool, which corresponds to tray
-        command_connection.perform_simple_code("T{}".format(self.tray_number))
-        # Send move command
-        commanded_move = dir*distance
-        if both == True:
-            command_connection.perform_simple_code("G0 E0:{}:{} F{}".format(commanded_move, commanded_move, feedrate))
-        else:
-            command_connection.perform_simple_code("G0 E0:{} F{}".format(commanded_move, feedrate))
-        # Disallow cold extrusion
-        command_connection.perform_simple_code("M302 P0")
-        # Set extruder absolute mode
-        command_connection.perform_simple_code("M82")
-    def move_motor_to_condition(self, motor, sensor, state):
+    # TODO: Function to execute moves asynchronusly
+    def execut_moves(self):
+        # command_connection = CommandConnection(debug=False)
+        # command_connection.connect()
+        # # Allow cold extrusion
+        # command_connection.perform_simple_code("M302 P1")
+        # # Set extruder relative mode
+        # command_connection.perform_simple_code("M83")
+        # # Select proper motor, or both if specified
+        # if both == True:
+        #     command_connection.perform_simple_code("M563 P0 D{}:{}:{} H{} F{}".format(self.tray_number, self.tray_motor_0, self.tray_motor_1, self.tray_number, self.tray_number))
+        # else:
+        #     command_connection.perform_simple_code("M563 P0 D{}:{} H{} F{}".format(self.tray_number, motor, self.tray_number, self.tray_number))
+        # # Select tool, which corresponds to tray
+        # command_connection.perform_simple_code("T{}".format(self.tray_number))
+        # # Send move command
+        # commanded_move = dir*distance
+        # if both == True:
+        #     command_connection.perform_simple_code("G0 E0:{}:{} F{}".format(commanded_move, commanded_move, feedrate))
+        # else:
+        #     command_connection.perform_simple_code("G0 E0:{} F{}".format(commanded_move, feedrate))
+        # # Disallow cold extrusion
+        # command_connection.perform_simple_code("M302 P0")
+        # # Set extruder absolute mode
+        # command_connection.perform_simple_code("M82")
         pass
--- tray_management.py
+++ tray_management.py
@@ -7,11 +7,5 @@ from tray_abstract import tray, Direction
 if __name__ == "__main__":
     extruder_0_tray = tray(0)
 
-    extruder_0_tray.move_motor_by(False, extruder_0_tray.tray_motor_0, Direction.FORWARD, 200, 2000)
-    extruder_0_tray.move_motor_by(True, extruder_0_tray.tray_motor_0, Direction.FORWARD, 200, 2000)
-    extruder_0_tray.move_motor_by(False, extruder_0_tray.tray_motor_1, Direction.FORWARD, 200, 2000)
-
-    time.sleep(2000)
-
     while(True):
         time.sleep(1)
